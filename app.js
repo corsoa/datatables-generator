855e198f5154eef3f6f92acee2c80c307352f02a
Aaron Corso <aaron@corsoa.com>
2015-07-29 22:17:21 -0400

    Adjust port for heroku deployment

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.get('/ajax', function(req, res, next) {
     res.send(response);
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 
     var host = server.address().address;
     var port = server.address().port;
